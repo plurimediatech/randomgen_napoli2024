94b67bacfc2c4e80bfbe75f03c88c75b4cde6050
lorenzo galotti <lorenzo.galotti@plurimedia.it>
2024-09-19 16:44:16 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  <div id="app">
+  <h1>Random Number Generator</h1>
+  <div>
+  <label for="min">Min:</label>
+  <input type="number" v-model.number="min" id="min" />
+  </div>
+  <div>
+  <label for="max">Max:</label>
+  <input type="number" v-model.number="max" id="max" />
+  </div>
+  <button @click="generateRandomNumber">Generate Random Number</button>
+  <p v-if="randomNumber !== null">Random Number: {{ randomNumber }}</p>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+  data() {
+  return {
+  min: 1,
+  max: 100,
+  randomNumber: null,
+  };
+  },
+  methods: {
+  generateRandomNumber() {
+  if (this.min < this.max) {
+  this.randomNumber = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+  } else {
+  alert("Min should be less than Max");
   }
-}
-</script>
-
-<style>
-#app {
+  },
+  },
+  };
+  </script>
+  
+  <style>
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
-}
-</style>
+  }
+  label {
+  margin-right: 10px;
+  }
+  input {
+  margin-bottom: 10px;
+  }
+  button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  }
+  </style>
